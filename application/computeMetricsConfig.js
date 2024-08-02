3e8cd1b0b6b25726af16e31968fbd3c749006b36
jonaslanzlinger <laenzlinger.jonas@gmail.com>
2024-08-02 10:40:24 +0200

    ft: add config for computing metrics (directories to exclude from computation)

---
+++ application/computeMetricsConfig.js
@@ -0,0 +1,8 @@
+/**
+ * @file computeMetricsConfig.js
+ * @description This file contains the configuration for computing the metrics,
+ * such as the excluded directories and the file extensions to consider.
+ */
+const excludedDirectories = ["node_modules", "dist"];
+
+export { excludedDirectories };
