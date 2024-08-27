794d5608e26f9dce045a2a25a5d5406e643dc6a1
jonaslanzlinger <laenzlinger.jonas@gmail.com>
2024-08-27 21:01:04 +0200

    test: add basic test

---
+++ backend/src/test/java/ch/unisg/backend/domain/RepoTests.java
@@ -0,0 +1,27 @@
+package ch.unisg.backend.domain;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+class RepoTests {
+
+    private static URL url;
+
+    @BeforeAll
+    static void setupUrl() {
+        try {
+            url = new URL("https://www.example.com");
+        } catch(MalformedURLException e) {
+            System.out.println("URL is malformed");
+        }
+    }
+
+    @Test
+    void newRepoTest() {
+        Repo repo = new Repo(url);
+        assert(repo.getStatus().equals(Repo.Status.REGISTERED));
+    }
+}
