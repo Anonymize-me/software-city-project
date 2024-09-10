be81326cd747e14b47970c98afd5f2128d836dbc
jonaslanzlinger <laenzlinger.jonas@gmail.com>
2024-09-10 23:57:12 +0200

    ref: improve dragging frames logic

--- frontend/js/user-interface/draggable-frames.js
+++ frontend/js/user-interface/draggable-frames.js
@@ -1,80 +1,36 @@
-const frameInfo = document.getElementById('frame-info');
 const frameModelTree = document.getElementById('frame-model-tree');
+const frameInfo = document.getElementById('frame-info');
 
-let frameInfoOffsetLeft = 0,
-   frameInfoOffsetTop = 0;
+let draggingElement = null;
+let isDragging = false;
+let offsetX, offsetY;
 
-frameInfo.addEventListener('mousedown', (e) => {
-   let rect = frameInfo.getBoundingClientRect();
-   frameInfoOffsetLeft = e.clientX - (rect.left + rect.width / 2);
-   frameInfoOffsetTop = e.clientY - (rect.top + rect.height / 2);
-   document.onmouseup = removeDrag;
-   if (
-      !(
-         frameInfoOffsetLeft > rect.width / 2 - 17 &&
-         frameInfoOffsetTop > rect.height / 2 - 17
-      )
-   ) {
-      document.onmousemove = dragFrameInfo;
+const draggingMouseDownEventListener = (draggable) => {
+   draggable.addEventListener('mousedown', (e) => {
+      const rect = draggable.getBoundingClientRect();
+      offsetX = e.clientX - rect.left;
+      offsetY = e.clientY - rect.top;
+      if (offsetX > rect.width - 17 && offsetY > rect.height - 17) return;
+      draggingElement = draggable;
+      isDragging = true;
+      document.addEventListener('mousemove', dragging);
+      document.addEventListener('mouseup', stopDragElement);
+   });
+}
+
+const dragging = (e) => {
+   if (isDragging) {
+      draggingElement.style.left = e.clientX - offsetX + 'px';
+      draggingElement.style.top = e.clientY - offsetY + 'px';
    }
-});
-
-let frameModelTreeOffsetLeft = 0,
-   frameModelTreeOffsetTop = 0;
-frameModelTree.addEventListener('mousedown', (e) => {
-   let rect = frameModelTree.getBoundingClientRect();
-   frameModelTreeOffsetLeft = e.clientX - (rect.left + rect.width / 2);
-   frameModelTreeOffsetTop = e.clientY - (rect.top + rect.height / 2);
-   document.onmouseup = removeDrag;
-   if (
-      !(
-         frameModelTreeOffsetLeft > rect.width / 2 - 17 &&
-         frameModelTreeOffsetTop > rect.height / 2 - 17
-      )
-   ) {
-      document.onmousemove = dragFrameModelTree;
-   }
-});
-
-const removeDrag = (e) => {
-   document.onmouseup = null;
-   document.onmousemove = null;
 };
 
-const dragFrameInfo = (e) => {
-   e.preventDefault();
-   let rect = frameInfo.getBoundingClientRect();
-   let newPosLeft = e.clientX - frameInfoOffsetLeft;
-   let newPosTop = e.clientY - frameInfoOffsetTop;
-   newPosLeft = newPosLeft - rect.width / 2 < 0 ? rect.width / 2 : newPosLeft;
-   newPosTop = newPosTop - rect.height / 2 < 0 ? rect.height / 2 : newPosTop;
-   newPosLeft =
-      newPosLeft + rect.width / 2 > window.innerWidth
-         ? window.innerWidth - rect.width / 2
-         : newPosLeft;
-   newPosTop =
-      newPosTop + rect.height / 2 > window.innerHeight
-         ? window.innerHeight - rect.height / 2
-         : newPosTop;
-   frameInfo.style.left = newPosLeft + 'px';
-   frameInfo.style.top = newPosTop + 'px';
+const stopDragElement = () => {
+   draggingElement = null;
+   isDragging = false;
+   document.removeEventListener('mousemove', dragging);
+   document.removeEventListener('mouseup', stopDragElement);
 };
 
-const dragFrameModelTree = (e) => {
-   e.preventDefault();
-   let rect = frameModelTree.getBoundingClientRect();
-   let newPosLeft = e.clientX - frameModelTreeOffsetLeft;
-   let newPosTop = e.clientY - frameModelTreeOffsetTop;
-   newPosLeft = newPosLeft - rect.width / 2 < 0 ? rect.width / 2 : newPosLeft;
-   newPosTop = newPosTop - rect.height / 2 < 0 ? rect.height / 2 : newPosTop;
-   newPosLeft =
-      newPosLeft + rect.width / 2 > window.innerWidth
-         ? window.innerWidth - rect.width / 2
-         : newPosLeft;
-   newPosTop =
-      newPosTop + rect.height / 2 > window.innerHeight
-         ? window.innerHeight - rect.height / 2
-         : newPosTop;
-   frameModelTree.style.left = newPosLeft + 'px';
-   frameModelTree.style.top = newPosTop + 'px';
-};
+draggingMouseDownEventListener(frameModelTree);
+draggingMouseDownEventListener(frameInfo);
